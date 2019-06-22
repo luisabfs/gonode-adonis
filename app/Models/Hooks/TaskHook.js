db631f4bdd6909c46bf6dfba0261dad2009fa244
Luísa Barros <qlullyq@gmail.com>
2019-06-22 00:05:30 -0300

    feat(task): send email via hooks

--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -6,9 +6,28 @@ const Helpers = use("Helpers");
 const TaskHook = (exports = module.exports = {});
 
 TaskHook.sendNewTaskMail = async taskInstance => {
-  if (!taskInstance.user_id || !taskInstance.dirty.user_id) {
+  if (!taskInstance.user_id && !taskInstance.dirty.user_id) {
     return;
   }
 
-  console.log("MAIL");
+  const { email, username } = await taskInstance.user().fetch();
+  const file = await taskInstance.file().fetch();
+  const { title } = taskInstance;
+
+  await Mail.send(
+    ["emails.new_task"],
+    { username, title, hasAttachment: !!file },
+    message => {
+      message
+        .to(email)
+        .from("qlullyq@gmail.com", "Luísa | ButterFly")
+        .subject("New task to you");
+
+      if (file) {
+        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
+          filename: file.name
+        });
+      }
+    }
+  );
 };
